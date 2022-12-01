86ee42dd09fcd928b514644ba092ee57629d7038
shinsanghooon <shinsanghooon@gmail.com>
2022-12-01 23:26:06 +0900

    아이템 완료하기
    - 완료 체크된 아이템 처리하기
    - 완료된 아이템에 대해 취소선 처리와 색상 변경 작업 진행

--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaTrash } from 'react-icons/fa';
 
 export default function Todo({ todo }) {
+	const [checked, setChecked] = useState(false);
+
+	const handleCheck = (e) => {
+		setChecked(e.target.checked);
+	};
+
+	const todoStyle = {
+		color: 'gray',
+		textDecoration: 'line-through',
+		textDecorationStyle: 'solid',
+	};
+
 	return (
 		<div className='todolist'>
 			<div>
-				<input type='checkbox' />
-				<span>{todo}</span>
+				<input type='checkbox' onClick={handleCheck} />
+				<span style={checked ? todoStyle : {}}>{todo}</span>
 			</div>
 			<button>
 				<FaTrash />
